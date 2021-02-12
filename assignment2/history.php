ebc489af5c8132b62c45a7c20a69e1cba7f63308
Hanh <57729860+Hanh-hub@users.noreply.github.com>
2021-02-11 18:37:05 -0600

    Add files via upload
     Front end history page reading from back end quotes database displaying quotes associated with USERID.

--- assignment2/history.php
+++ assignment2/history.php
@@ -1,26 +1,63 @@
-<?php
-    if (isset($_POST["profilesubmit"]))
-    {
-        $fullname=$_POST["fname"];
-        $address=$_POST["address1"];
-        $altaddress=$_POST["address2"];
-        $city=$_POST["city"];
-        $state=$_POST["state"];
-        $zip=$_POST["zip"];
-        $USERID= $_SESSION['USERID'];
-    }
-    else
-    {
-        $fullname="fname";
-        $address="address1";
-        $altaddress="address2";
-        $city="city";
-        $state="state";
-        $zip= 44123;
-        $USERID=10;
-    }
-    $test='UPDATE profiles set FULLNAME="'.$fullname.'",ADDRESS1="'.$address.'",ADDRESS2="'.$altaddress.'",CITY="'.$city.'",ST="'.$state.'",ZIP='.$zip.' WHERE USERID='.$USERID.';';
-    echo '<p>' .$test. '</p>';
 
+<?php
+
+include_once 'navbar.php'
 
 ?>
+
+<div class="history">
+    <table border='1'  style="height:100%;width:100%; position: relative; top: 0; bottom: 0; left: 0; right: 0;border:1px solid">
+    <tr>
+        <td>QUOTEID</td>
+        <td>USERID</td>
+        <td>Gallons</td>
+        <td>Address</td>
+        <td>Scheduled Date</td>
+        <td>Suggested Price</td>
+        <td>Total Cost</td>
+    </tr>
+    <?php
+        require_once 'includes/dbh.inc.php';
+        $result = mysqli_query($conn, "SELECT * FROM quotes WHERE USERID=".$_SESSION['USERID'].";");
+        while($row = mysqli_fetch_array($result))
+        {
+            echo "<tr>";
+            echo "<td>" . $row['QUOTEID'] . "</td>";
+            echo "<td>" . $row['USERID'] . "</td>";
+            echo "<td>" . $row['GALLONS'] . "</td>";
+            echo "<td>" . $row['ADDRESS1'] . "</td>";
+            echo "<td>" . $row['SCHEDDATE'] . "</td>";
+            echo "<td>" . $row['SUGGESTED'] . "</td>";
+            echo "<td>" . $row['TOTAL'] . "</td>";
+            echo "</tr>";
+        }
+
+    ?>
+    </table>
+</div>
+
+<script>
+    // Get the modal
+    var modal = document.getElementById('id02');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    
+</script>
+<script type="text/javascript">
+    document.getElementById('profileForm').style.visibility='hidden'
+    var theButton = document.getElementById('profileButton');
+    
+    theButton.onclick = function() { 
+        document.getElementById('profileForm').style.visibility='visible';   
+    }
+    
+</script>
+
+
+</body>
+</html> 
